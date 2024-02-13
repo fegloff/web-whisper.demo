d6d2886acc7ce60b76a052c395f2d2dcf9ebed5f
fegloff <fegloff@gmail.com>
2024-02-13 00:50:30 -0500

    add  SpeechRecognition

--- src/components/RecordingComponent.styles.tsx
+++ src/components/RecordingComponent.styles.tsx
@@ -8,6 +8,21 @@ export const AudioContainer = styled.div`
   width: 100% !important;
   gap: 1em;
 
+  .recorder-panel {
+    display: flex;
+    flex-direction: row;
+    align-items: center !important;
+    justify-content: center;
+    width: 100% !important;
+    gap: 3em;
+  }
+
+  .mic-panel {
+    display: flex;
+    flex-direction: column;
+    align-items: center !important;
+  }
+
   .transcription-container {
     border: 1px solid gray;
     border-radius: 15px;
--- src/components/RecordingComponent.tsx
+++ src/components/RecordingComponent.tsx
@@ -1,41 +1,86 @@
+import React, { useEffect, useState } from "react";
+import { AudioRecorder } from "react-audio-voice-recorder";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import { speechToText } from "../api/openai";
 
-import { AudioRecorder } from 'react-audio-voice-recorder';
-import { speechToText } from '../api/openai';
-import React, { useState } from 'react'
-import { AudioContainer } from './RecordingComponent.styles';
+import { AudioContainer } from "./RecordingComponent.styles";
 
 const RecordingComponent = () => {
-  const [transcription, setTranscription] = useState('')
+  const [transcription, setTranscription] = useState("");
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
 
-  const addAudioElement = async (blob: Blob | MediaSource) => {
+  useEffect(() => {
+    if (transcript) {
+      setTranscription(transcript);
+    }
+  }, [transcript]);
+  
+  const updateTranscriptionWithWhisper = async (blob: Blob | MediaSource) => {
     try {
       if (blob) {
-        const response = await speechToText(blob as Blob)
+        const response = await speechToText(blob as Blob);
         if (transcription) {
-          setTranscription(transcription.concat(`\n${response}`))
+          setTranscription(transcription.concat(`\n${response}`));
         } else {
-          setTranscription(response)
+          setTranscription(response);
         }
       }
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
   };
-  
+
+  const resetTranscription = () => {
+    setTranscription("")
+    resetTranscript()
+  }
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
   return (
     <AudioContainer>
-        <AudioRecorder 
-        onRecordingComplete={addAudioElement}
-        audioTrackConstraints={{
-          noiseSuppression: true,
-          echoCancellation: true,
-        }} 
-        // downloadOnSavePress={true}
-        downloadFileExtension="mp3"
-      />
-      <div className='transcription-container'>{transcription}</div>
-      <div><button onClick={() => setTranscription('')}>Clear</button></div>
-    </AudioContainer>)
-}
+      <div className='recorder-panel'>
+        <div className="mic-panel">
+          <p>Using Whisper</p>
+          <AudioRecorder
+            onRecordingComplete={updateTranscriptionWithWhisper}
+            audioTrackConstraints={{
+              noiseSuppression: true,
+              echoCancellation: true,
+            }}
+            // downloadOnSavePress={true}
+            downloadFileExtension="mp3"
+          />
+        </div>
+        <div className="mic-panel">
+          <p>Using SpeechRecognition</p>
+          <button
+            onClick={() => {
+              listening
+                ? SpeechRecognition.stopListening()
+                : SpeechRecognition.startListening();
+            }}
+          >
+            {listening ? 'recording' : 'record'}
+          </button>
+        </div>
+      </div>
 
-export default RecordingComponent
+      <div className="transcription-container">{transcription}</div>
+      <div>
+        <button onClick={resetTranscription}>Clear</button>
+      </div>
+    </AudioContainer>
+  );
+};
+
+export default RecordingComponent;
